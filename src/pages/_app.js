34f14fcaa00074b237bdac29f06b133e957fcb2e
matthew <matthewbudiman@outlook.com>
2021-10-15 00:45:34 -0500

    added h1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
       <div className="p-5 w-screen h-screen">
+        <h1>HELLO</h1>
         <Component {...pageProps} />
       </div>
     </UserProvider>
