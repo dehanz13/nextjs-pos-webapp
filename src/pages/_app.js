559a3017ea385737a93e018417c6552d280ee554
matthew <matthewbudiman@outlook.com>
2021-10-15 00:48:09 -0500

    testing openshift

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
       <div className="p-5 w-screen h-screen">
-        <h1>HELLO</h1>
+        <h1>HELLO THERE!!</h1>
         <Component {...pageProps} />
       </div>
     </UserProvider>
