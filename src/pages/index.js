af503991cabf2ba46ae6db901991de8d20b6c954
dehanz13 <hansel.danniel@gmail.com>
2021-10-18 22:49:31 -0500

    edited index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-blue-500">
-      <div className="m-auto">I LIKE APPLE</div>
+    <div className="h-screen w-screen bg-blue-400">
+      <div className="m-auto">I LIKE APPLE!!!</div>
     </div>
   );
 }
